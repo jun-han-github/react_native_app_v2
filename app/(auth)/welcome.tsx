fa38e0f675c1898bd855014f284f16014eac5f06
Junhan <junhan.3008@gmail.com>
2024-09-08 21:41:07 +0800

    feat: CustomButton

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,7 +1,8 @@
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
@@ -12,6 +13,7 @@ const Onboarding = () => {
 
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
     <SafeAreaView className="flex h-full items-center justofy-between bg-white">
@@ -34,11 +36,39 @@ const Onboarding = () => {
         onIndexChanged={(index) => setActiveIndex(index)}
       >
         {onboarding.map((item) => (
-          <View>
-            <Text key={item.id}>{item.title}</Text>
+          <View className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text
+                key={item.id}
+                className="text-black text-3xl font-bold mx-10 text-center"
+              >
+                {item.title}
+              </Text>
+            </View>
+            <Text
+              key={item.id}
+              className="text-lg text-center text-[#858585] mx-10 mt-3"
+            >
+              {item.description}
+            </Text>
           </View>
         ))}
       </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
