e6778517d1c987ce132e6aa9cf00679961ea6b5a
Junhan <junhan.3008@gmail.com>
2024-09-08 22:22:37 +0800

    feat: sign up page input fields & custom component InputField

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,7 +1,19 @@
-import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignupPress = async () => {};
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -10,6 +22,64 @@ const SignUp = () => {
           <Text className="text-2xl text-black absolute bottom-5 left-5">
             Create Your Account
           </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) =>
+              setForm({
+                ...form,
+                name: value,
+              })
+            }
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) =>
+              setForm({
+                ...form,
+                email: value,
+              })
+            }
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry={true}
+            onChangeText={(value) =>
+              setForm({
+                ...form,
+                password: value,
+              })
+            }
+          />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignupPress}
+            className="mt-6"
+          />
+
+          {/* OAuth Google */}
+
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account?</Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+
+          {/* Verification Modal */}
         </View>
       </View>
     </ScrollView>
