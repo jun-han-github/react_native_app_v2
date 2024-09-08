ce9e2387da236173333f5ef4add434f2e739c613
Junhan <junhan.3008@gmail.com>
2024-09-08 21:51:29 +0800

    feat: signup page header

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,18 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>Sign Up</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
