1c237c2b27bb1a12b2950ab13f5edf798888bf75
Junhan <junhan.3008@gmail.com>
2024-09-09 21:58:57 +0800

    feat: Google OAuth button in signup page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
 import { Link } from "expo-router";
 import { useState } from "react";
@@ -69,7 +70,7 @@ const SignUp = () => {
             className="mt-6"
           />
 
-          {/* OAuth Google */}
+          <OAuth />
 
           <Link
             href="/sign-in"
