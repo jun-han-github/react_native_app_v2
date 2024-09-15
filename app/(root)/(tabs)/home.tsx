5979de78a63f791653318dfb85cf50433e5343f6
Junhan <junhan.3008@gmail.com>
2024-09-15 17:29:13 +0800

    fix: font bold

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -161,7 +161,7 @@ export default function Page() {
             />
 
             <>
-              <Text className="text-xl mt-5 mb-3">
+              <Text className="text-xl font-bold mt-5 mb-3">
                 Your Current Location
               </Text>
               <View className="flex flex-row items-center bg-transparent h-[300px]">
