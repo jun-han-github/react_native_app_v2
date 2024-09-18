7b54aa00fbb093e232c3a4327027a956c2ef0fbe
Junhan <junhan.3008@gmail.com>
2024-09-18 22:42:29 +0800

    feat: confirm-ride layout

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,14 @@
+import RideLayout from "@/components/RideLayout";
+import { Text, View } from "react-native";
+
+const ConfirmRide = () => {
+    return (
+        <RideLayout title="Ride">
+            <View>
+                <Text>Confirm Ride</Text>
+            </View>
+        </RideLayout>
+    );
+}
+
+export default ConfirmRide;
