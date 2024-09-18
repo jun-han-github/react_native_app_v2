88614700fa21734537119cd8c06033c9e13434c8
Junhan <junhan.3008@gmail.com>
2024-09-18 22:08:15 +0800

    feat: find-ride layout & map

---
+++ components/RideLayout.tsx
@@ -0,0 +1,30 @@
+import { icons } from "@/constants";
+import { router } from "expo-router";
+import { ReactNode } from "react"
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Map from "./Map";
+
+const RideLayout = ({ title, children }: { title?: string; children: ReactNode }) => {
+    return <GestureHandlerRootView>
+        <View className="flex-1 bg-white">
+            <View className="flex flex-col h-screen bg-blue-500">
+                <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <View className="w-10 h-10 bg-white rounded-full items-center jsutify-center">
+                            <Image
+                                source={icons.backArrow}
+                                resizeMode="contain"
+                                className="w-6 h-6"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                    <Text className="text-xl ml-5">{ title || 'Go Back' }</Text>
+                </View>
+                <Map/>
+            </View>
+        </View>
+    </GestureHandlerRootView>
+};
+
+export default RideLayout;
