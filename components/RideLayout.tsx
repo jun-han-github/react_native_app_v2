e2b3e3c534c2f6e85575cc71e924d6bf5ebc094c
Junhan <junhan.3008@gmail.com>
2024-09-18 22:34:49 +0800

    fix: virtualized list should never be nested in scrollview

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -3,7 +3,7 @@ import { router } from "expo-router";
 import { ReactNode, useRef } from "react"
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import Map from "./Map";
 
 const RideLayout = ({ title, children }: { title?: string; children: ReactNode }) => {
@@ -32,9 +32,9 @@ const RideLayout = ({ title, children }: { title?: string; children: ReactNode }
                 snapPoints={['40%', '85%']}
                 index={0}
             >
-                <BottomSheetScrollView style={{ flex: 1, padding: 20 }}>
+                <BottomSheetView style={{ flex: 1, padding: 20 }}>
                     { children }
-                </BottomSheetScrollView>
+                </BottomSheetView>
             </BottomSheet>
         </View>
     </GestureHandlerRootView>
